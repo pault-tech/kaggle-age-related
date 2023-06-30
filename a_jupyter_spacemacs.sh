2a4258c5e06e075501ff9200d5c8b11fa9c8112f
Paul T <pault-tech@github.com>
2023-06-30 06:22:08 +0000

    update

--- a_jupyter_spacemacs.sh
+++ a_jupyter_spacemacs.sh
@@ -1,5 +1,5 @@
 #NOTE: required for spacemacs!!
-#for jupytext conversion to work in spacemacs need to
+#for jupytext conversion to work in spacemacs ipython-notebook layer need to
 #Have an IPython notebook running
+pip install --user jupytext #sync ipynb .py files; required for 
 jupyter notebook
-
