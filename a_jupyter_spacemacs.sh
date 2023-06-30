7d28bb57d8b70fb44fa5407dc48ff3a480cb47ac
Paul T <pault-tech@github.com>
2023-06-30 16:17:49 +0000

    works

--- a_jupyter_spacemacs.sh
+++ a_jupyter_spacemacs.sh
@@ -2,4 +2,4 @@
 #for jupytext conversion to work in spacemacs ipython-notebook layer need to
 #Have an IPython notebook running and jupytext installed (for conversion)
 pip install --user jupytext #sync ipynb .py files; required for 
-jupyter notebook
+TERM=xterm-256color jupyter notebook
