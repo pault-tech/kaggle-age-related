ef6f7b7cd8f061f39262bca145281a035f84f9c0
Paul T <pault-tech@github.com>
2023-06-30 18:41:07 +0000

    wip

--- a_jupyter_spacemacs.sh
+++ a_jupyter_spacemacs.sh
@@ -2,4 +2,7 @@
 #for jupytext conversion to work in spacemacs ipython-notebook layer need to
 #Have an IPython notebook running and jupytext installed (for conversion)
 pip install --user jupytext #sync ipynb .py files; required for 
-TERM=xterm-256color jupyter notebook
+#use --no-browser to not attempt to open notebook list in browser on startup as that causing error in codespaces
+TERM=xterm-256color jupyter --no-browser notebook
+
+#use (ein:notebooklist-open)
